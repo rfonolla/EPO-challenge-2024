2e8391af536af355e98978d179d64bf2406a316b
Roger <roger.fonolla@gmail.com>
2024-09-15 19:58:21 +0200

    Added input argument to main for json file

--- src/image_generation_pipeline.py
+++ src/image_generation_pipeline.py
@@ -2,16 +2,21 @@ import utils
 from llama_index.core.prompts import PromptTemplate
 from llama_index.core import Settings
 
-def generate_image_from_code(input_text, prompt_template):
+def generate_image_from_code(input_text, prompt_template, output_filename, print_prompt):
 
 
     input_prompt = PromptTemplate((prompt_template))
+
+    if print_prompt:
+        print(input_prompt.format(output_filename=output_filename,information=input_text))
     
     # Query the LLM to generate the summary or answer
-    summary = Settings.llm.complete(input_prompt.format(information=input_text))
-    print(summary.text)
-    
+    summary = Settings.llm.complete(input_prompt.format(output_filename=output_filename,
+                                                        information=input_text
+                                                       )
+                                   )    
     result = utils.get_code_from_text(summary.text)
+        
     try:
         exec(result)
         print("Code executed successfully")
--- src/main.py
+++ src/main.py
@@ -5,19 +5,30 @@ import utilsEPO
 import warnings
 from image_generation_pipeline import *
 from login_claude import *
+import argparse
 import json
+import sys
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-if __name__ == "__main__":
 
 
-    # Read the JSON file
-    with open('config.json', 'r') as file:
-        args = json.load(file)
+def load_config(file_path):
+    try:
+        with open(file_path, 'r') as config_file:
+            return json.load(config_file)
+    except json.JSONDecodeError:
+        print(f"Error: {file_path} is not a valid JSON file.")
+        sys.exit(1)
+    except IOError:
+        print(f"Error: Could not read file {file_path}")
+        sys.exit(1)
+
+def main(args):
 
     # Initialize the appropriate LLM based on the model name
     model_llm = args['model_llm']
+    
     if 'claude' in model_llm.lower():
         llm = Anthropic(model=model_llm, temperature=args['temperature'], max_tokens=args['max_tokens']) # ["claude-3-5-sonnet-20240620","claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307","gpt-3.5-turbo"]
     elif 'gpt' in model_llm.lower():
@@ -40,6 +51,9 @@ if __name__ == "__main__":
     print('Detecting numbers from selected image')
     if dict_args['patent_images']:
         numbers_claim = utils.get_numbers_from_Image(data_patent['images'])
+
+    print(args['print_prompt'])
+    print(args['output_filename'])
     
     print('Running RAG Pipeline')
     output_result = run_RAG_pipeline(llm=llm,
@@ -47,10 +61,21 @@ if __name__ == "__main__":
                                      claim_text=data_patent['claim_text'], 
                                      description_text=data_patent['description_text'],
                                      print_prompt=args['print_prompt'])
-    print(output_result)
     
-    image_code = generate_image_from_code(output_result, 
-                                          prompt_template=args['prompt_template_image'])                        
+    _ = generate_image_from_code(output_result, 
+                                 prompt_template=args['prompt_template_image'],
+                                 output_filename = args['output_filename'],
+                                 print_prompt=args['print_prompt'])                        
+    
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Runs claude with an input config JSON file.")
+    parser.add_argument("-i", "--input_json", required=True, help="Path to the JSON config file")
+    args = parser.parse_args()
+
+    config = load_config(args.input_json)
+    
+    main(config)
 
         
     
